3c10e93ba9276826c65af733e8a4501fe8be9ce2
ik013043z1 <ik013043z1@H013043.NET>
2018-11-12 09:49:54 +0100

    5.3

---
+++ src/MultipleArrayList.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MultipleArrayList {
+	public static void main(String[] args) {
+		
+	}
+}
