7b1cac6183fe2619128113bcd1f1bbc51887e846
ik013043z1 <ik013043z1@H013043.NET>
2018-11-12 09:20:13 +0100

    5.1

--- src/ObjectToAL.java
+++ src/ObjectToAL.java
@@ -1,13 +1,17 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class ObjectToAL {
 	public static void main(String[] args) {
 		
+		// Declare the ArrayList & the ListIterator.
 		ArrayList<FootballMatch> Matches = new ArrayList<FootballMatch>();
+		ListIterator<FootballMatch> lite = null;
 		
+		// Some lines to obligate the user to enter only one argument.
 		if (args.length > 1 ) {
 			System.out.println("You enter more than one argument. Bye!");
 		}
@@ -38,17 +42,46 @@ public class ObjectToAL {
 					
 					// Add all variables in the ArrayList.
 					Matches.add(fmatch);
-					}
-						
 				}
+						
+			}
 
+			// The exception.
 			catch (FileNotFoundException e) {
 				System.out.println("File not found...");
 				
 			}
 
-			System.out.println("There are " + Matches.size() + " matches in the ArrayList.");
+			// List of all the matches:
+			System.out.println("All matches:");
+			
+			System.out.println("There are " + Matches.size() + " matches.");
 		
+			for (int x = 0; Matches.size() - 1 >= x; x++) {
+				System.out.println(Matches.get(x).getLocalTeam() + " " + Matches.get(x).getGoalsLocal() + " vs " + Matches.get(x).getVisitorTeam() + " " + Matches.get(x).getGoalsVisitor());
+			}
+			
+			System.out.println("");
+			
+			// List of the draws, and deleting from the list.
+			System.out.println("The matches finished in draw:");
+			
+			lite = Matches.listIterator();
+			FootballMatch draw = lite.next();
+			
+			while(lite.hasNext()) {
+				if(draw.getGoalsLocal() == draw.getGoalsVisitor()) {
+					System.out.println(draw.getLocalTeam() + " " + draw.getGoalsLocal() + " vs " + draw.getVisitorTeam() + " " + draw.getGoalsVisitor());
+					
+					lite.remove();
+				}
+			}
+			
+			System.out.println("");
+			
+			// List of all the matches without the draws.
+			System.out.println("The matches finished with the victory of one of the teams:");
+			
 			for (int x = 0; Matches.size() - 1 >= x; x++) {
 				System.out.println(Matches.get(x).getLocalTeam() + " " + Matches.get(x).getGoalsLocal() + " vs " + Matches.get(x).getVisitorTeam() + " " + Matches.get(x).getGoalsVisitor());
 			}
