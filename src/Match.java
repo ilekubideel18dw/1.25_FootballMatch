69857f2cc0630f1764e42ef999ea0796a4d844e6
ik013043z1 <ik013043z1@H013043.NET>
2018-11-23 11:32:13 +0100

    7.3

---
+++ src/Match.java
@@ -0,0 +1,56 @@
+public class Match {
+
+	private Team localTeam;
+	private Team visitorTeam;
+	private int goalsLocal;
+	private int goalsVisitor;
+
+	public Team getLocalTeam() {
+	
+		return localTeam;
+		
+    }
+
+	public void setLocalTeam(Team LocalTeam) {
+		
+		this.localTeam = LocalTeam;
+		
+	}
+
+	public Team getVisitorTeam() {
+		
+		return visitorTeam;
+		
+	}
+
+	public void setVisitorTeam(Team visitorTeam) {
+		
+		this.visitorTeam = visitorTeam;
+		
+	}
+
+	public int getGoalsLocal() {
+		
+		return goalsLocal;
+		
+	}
+
+	public void setGoalsLocal(int goalsLocal) {
+		
+		this.goalsLocal = goalsLocal;
+		
+	}
+
+	public int getGoalsVisitor() {
+		
+		return goalsVisitor;
+		
+	}
+
+	public void setGoalsVisitor(int goalsVisitor) {
+		
+		this.goalsVisitor = goalsVisitor;
+		
+	}
+	
+}
--- src/ReadTeams.java
+++ src/ReadTeams.java
@@ -20,40 +20,11 @@ public class ReadTeams {
 		}
 		
 		else {
-
-			try {
-				
-				// File = Argument & enter the file in Scanner.
-				File file01 = new File(args[0]);
-				Scanner sm = new Scanner(file01);
-				
-				while(sm.hasNextLine()) {
-					String lineF = sm.nextLine();
-					String[] wordF = lineF.split("::");
-					
-					FootballMatch fmatch = new FootballMatch();	
-					
-					// Add variable per variable in a variable.
-					fmatch.setLocalTeam(wordF[0]);
-					fmatch.setVisitorTeam(wordF[1]);
-					fmatch.setGoalsLocal(Integer.parseInt(wordF[2]));
-					fmatch.setGoalsVisitor(Integer.parseInt(wordF[3]));
-					
-					// Add all variables in the ArrayList.
-					matches.add(fmatch);
-				}
-						
-			}
-
-			// The exception.
-			catch (FileNotFoundException e) {
-				System.out.println("File not found...");
-			}
 			
 			try {
 				
 				// File = Argument & enter the file in Scanner.
-				File file02 = new File(args[1]);
+				File file02 = new File(args[0]);
 				Scanner st = new Scanner(file02);
 				
 				while(st.hasNextLine()) {
@@ -72,30 +43,54 @@ public class ReadTeams {
 					// Add all variables in the ArrayList.
 					footballteams.add(fteam);
 				}
-					// footballteams.get(y).getTeamName() + " " + footballteams.get(y).getTeamCountry() + " " + footballteams.get(y).getTeamStadium() + " " + footballteams.get(y).getTeamLeague() + " " + footballteams.get(y).getTeamFoundationYear()
+				
 			}
 
 			// The exception.
 			catch (FileNotFoundException e) {
 				System.out.println("File not found...");
 			}
+			
+			try {
+				
+				// File = Argument & enter the file in Scanner.
+				File file = new File(args[0]);
+				Scanner sc = new Scanner(file);
+				
+				while(sc.hasNextLine()) {
+					String line = sc.nextLine();
+					String[] word = line.split("::");
+					
+					FootballMatch fmatch = new FootballMatch();	
+					
+					// Add variable per variable in a variable.
+					
+					for (int a = 0; a < footballteams.size(); a++) {
+						if (word[0].matches(footballteams.get(a).getTeamName())) {
+							fmatch.setLocalTeam(footballteams.get(a));
+						}
+						
+						else if (word[1].matches(footballteams.get(a).getTeamName()))
+							fmatch.setVisitorTeam(footballteams.get(a));
+						}
+
+					fmatch.setGoalsLocal(Integer.parseInt(word[2]));
+					fmatch.setGoalsVisitor(Integer.parseInt(word[3]));
+					
+					// Add all variables in the ArrayList.
+					matches.add(fmatch);
+				}
+						
+			}
+
+			// The exception.
+			catch (FileNotFoundException e) {
+				System.out.println("File not found...");
+				
+			}
 
 			// Quantity of matches & teams:			
 			System.out.println("There are " + matches.size() + " matches.");
-			System.out.println("There are " + footballteams.size() + " teams.");
-		
-			for (int x = 0; matches.size() - 1 >= x; x++) {
-				for (int y = 0; footballteams.size() - 1 >= y; y++) {
-					if (matches.get(x).getLocalTeam() == footballteams.get(y).getTeamName() || matches.get(x).getVisitorTeam() == footballteams.get(y).getTeamName()) {
-						System.out.println(footballteams.get(y).getTeamName() + " " + footballteams.get(y).getTeamCountry() 
-								+ " " + footballteams.get(y).getTeamStadium() + " " + footballteams.get(y).getTeamLeague() 
-								+ " " + footballteams.get(y).getTeamFoundationYear() + " " + matches.get(x).getGoalsLocal() 
-								+ " vs " + footballteams.get(y).getTeamName() + " " + footballteams.get(y).getTeamCountry() 
-								+ " " + footballteams.get(y).getTeamStadium() + " " + footballteams.get(y).getTeamLeague() 
-								+ " " + footballteams.get(y).getTeamFoundationYear() + " " + matches.get(x).getGoalsVisitor());
-					}
-				}
-			}
 		}
 	}
 } 
